c26e070ab9d975fe3c69e10179bc0af5f6f8c45f
Sathwik puttala <sathwikputtala2003@gmail.com>
2026-09-06 22:41:00 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import { Routes, Route } from "react-router-dom";
+// pages
+import Home from "./Pages/Home/Home";
+import Video from "./Pages/Video/Video";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/video/:categoryId/:videoId" element={<Video />} />
+      </Routes>
     </div>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Home.css";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
 };
 
 export default Home;
